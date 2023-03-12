fbc43f7b661a76e7e928a982b7fd33c5d901fbb4
solita-michalguspiel <michal.guspiel@solita.fi>
2023-03-12 12:58:39 +0200

    Added CORS

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const bodyParser = require("body-parser");
+app.use(cors());
 app.use(
     bodyParser.urlencoded({
       extended: true,
